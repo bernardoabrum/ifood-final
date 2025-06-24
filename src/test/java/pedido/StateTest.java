12d3f228fb7751a4ca5a2faf8e0fd455979e38e5
Bernardo <bernardoalvesbrum_100@hotmail.com>
2025-06-23 23:48:54 -0300

    refactoring state test

--- src/test/java/pedido/StateTest.java
+++ src/test/java/pedido/StateTest.java
@@ -18,264 +18,266 @@ public class StateTest {
     }
 
     @Test
-    public void deveMudarParaEfetuado() {
+    public void deveIniciarCOmoEfetuado() {
         assertEquals("Pedido efetuado", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
     public void deveMudarDeEfetuadoParaAceito() {
+        pedido.setPedidoEstado(PedidoEstadoEfetuado.getInstance());
         pedido.aceitarPedido();
         assertEquals("Pedido aceito", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
     public void deveMudarDeEfetuadoParaNegado() {
+        pedido.setPedidoEstado(PedidoEstadoEfetuado.getInstance());
         pedido.negarPedido();
         assertEquals("Pedido negado", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEfetuadoParaPreparando(){
+    public void naoDeveMudarDeEfetuadoParaPreparando() {
         pedido.setPedidoEstado(PedidoEstadoEfetuado.getInstance());
         assertFalse(pedido.prepararPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEfetuadoParaEnviado(){
+    public void naoDeveMudarDeEfetuadoParaEnviado() {
         pedido.setPedidoEstado(PedidoEstadoEfetuado.getInstance());
         assertFalse(pedido.enviarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEfetuadoParaEntregue(){
+    public void naoDeveMudarDeEfetuadoParaEntregue() {
         pedido.setPedidoEstado(PedidoEstadoEfetuado.getInstance());
         assertFalse(pedido.entregarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEfetuadoParaCancelado(){
+    public void naoDeveMudarDeEfetuadoParaCancelado() {
         pedido.setPedidoEstado(PedidoEstadoEfetuado.getInstance());
         assertFalse(pedido.cancelarPedido());
     }
 
     @Test
-    public void deveMudarDeAceitoParaPreparando(){
+    public void deveMudarDeAceitoParaPreparando() {
         pedido.setPedidoEstado(PedidoEstadoAceito.getInstance());
         pedido.prepararPedido();
         assertEquals("Pedido sendo preparado", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
-    public void deveMudarDeAceitoParaCancelado(){
+    public void deveMudarDeAceitoParaCancelado() {
         pedido.setPedidoEstado(PedidoEstadoAceito.getInstance());
         pedido.cancelarPedido();
         assertEquals("Pedido cancelado", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
-    public void naoDeveMudarDeAceitoParaEfetuado(){
+    public void naoDeveMudarDeAceitoParaEfetuado() {
         pedido.setPedidoEstado(PedidoEstadoAceito.getInstance());
         assertFalse(pedido.efetuarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeAceitoParaNegado(){
+    public void naoDeveMudarDeAceitoParaNegado() {
         pedido.setPedidoEstado(PedidoEstadoAceito.getInstance());
         assertFalse(pedido.negarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeAceitoParaEnviado(){
+    public void naoDeveMudarDeAceitoParaEnviado() {
         pedido.setPedidoEstado(PedidoEstadoAceito.getInstance());
         assertFalse(pedido.enviarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeAceitoParaEntregue(){
+    public void naoDeveMudarDeAceitoParaEntregue() {
         pedido.setPedidoEstado(PedidoEstadoAceito.getInstance());
         assertFalse(pedido.entregarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeNegadoParaEfetuado(){
+    public void naoDeveMudarDeNegadoParaEfetuado() {
         pedido.setPedidoEstado(PedidoEstadoNegado.getInstance());
         assertFalse(pedido.efetuarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeNegadoParaAceito(){
+    public void naoDeveMudarDeNegadoParaAceito() {
         pedido.setPedidoEstado(PedidoEstadoNegado.getInstance());
         assertFalse(pedido.aceitarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeNegadoParaPreparando(){
+    public void naoDeveMudarDeNegadoParaPreparando() {
         pedido.setPedidoEstado(PedidoEstadoNegado.getInstance());
         assertFalse(pedido.prepararPedido());
     }
 
     @Test
-    public void naoDeveMudarDeNegadoParaEnviado(){
+    public void naoDeveMudarDeNegadoParaEnviado() {
         pedido.setPedidoEstado(PedidoEstadoNegado.getInstance());
         assertFalse(pedido.enviarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeNegadoParaEntregue(){
+    public void naoDeveMudarDeNegadoParaEntregue() {
         pedido.setPedidoEstado(PedidoEstadoNegado.getInstance());
         assertFalse(pedido.entregarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeNegadoParaCancelado(){
+    public void naoDeveMudarDeNegadoParaCancelado() {
         pedido.setPedidoEstado(PedidoEstadoNegado.getInstance());
         assertFalse(pedido.cancelarPedido());
     }
 
     @Test
-    public void deveMudarDePreparandoParaEnviado(){
+    public void deveMudarDePreparandoParaEnviado() {
         pedido.setPedidoEstado(PedidoEstadoPreparando.getInstance());
         pedido.enviarPedido();
         assertEquals("Pedido enviado", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
-    public void deveMudarDePreparandoParaCancelado(){
+    public void deveMudarDePreparandoParaCancelado() {
         pedido.setPedidoEstado(PedidoEstadoPreparando.getInstance());
         pedido.cancelarPedido();
         assertEquals("Pedido cancelado", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
-    public void naoDeveMudarDePreparandoParaEfetuado(){
+    public void naoDeveMudarDePreparandoParaEfetuado() {
         pedido.setPedidoEstado(PedidoEstadoPreparando.getInstance());
         assertFalse(pedido.efetuarPedido());
     }
 
     @Test
-    public void naoDeveMudarDePreparandoParaAceito(){
+    public void naoDeveMudarDePreparandoParaAceito() {
         pedido.setPedidoEstado(PedidoEstadoPreparando.getInstance());
         assertFalse(pedido.aceitarPedido());
     }
 
     @Test
-    public void naoDeveMudarDePreparandoParaNegado(){
+    public void naoDeveMudarDePreparandoParaNegado() {
         pedido.setPedidoEstado(PedidoEstadoPreparando.getInstance());
         assertFalse(pedido.negarPedido());
     }
 
     @Test
-    public void naoDeveMudarDePreparandoParaEntregue(){
+    public void naoDeveMudarDePreparandoParaEntregue() {
         pedido.setPedidoEstado(PedidoEstadoPreparando.getInstance());
         assertFalse(pedido.entregarPedido());
     }
 
     @Test
-    public void deveMudarDeEnviadoParaEntregue(){
+    public void deveMudarDeEnviadoParaEntregue() {
         pedido.setPedidoEstado(PedidoEstadoEnviado.getInstance());
         pedido.entregarPedido();
         assertEquals("Pedido recebido", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
-    public void deveMudarDeEnviadoParaCanceclado(){
+    public void deveMudarDeEnviadoParaCanceclado() {
         pedido.setPedidoEstado(PedidoEstadoEnviado.getInstance());
         pedido.cancelarPedido();
         assertEquals("Pedido cancelado", pedido.getPedidoEstado().getEstadoPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEnviadoParaEfetuado(){
+    public void naoDeveMudarDeEnviadoParaEfetuado() {
         pedido.setPedidoEstado(PedidoEstadoEnviado.getInstance());
         assertFalse(pedido.efetuarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEnviadoParaAceito(){
+    public void naoDeveMudarDeEnviadoParaAceito() {
         pedido.setPedidoEstado(PedidoEstadoEnviado.getInstance());
         assertFalse(pedido.aceitarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEnviadoParaNegado(){
+    public void naoDeveMudarDeEnviadoParaNegado() {
         pedido.setPedidoEstado(PedidoEstadoEnviado.getInstance());
         assertFalse(pedido.negarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEnviadoParaPreparado(){
+    public void naoDeveMudarDeEnviadoParaPreparado() {
         pedido.setPedidoEstado(PedidoEstadoEnviado.getInstance());
         assertFalse(pedido.prepararPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEntregueParaEfetuado(){
+    public void naoDeveMudarDeEntregueParaEfetuado() {
         pedido.setPedidoEstado(PedidoEstadoEntregue.getInstance());
         assertFalse(pedido.efetuarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEntregueParaAceito(){
+    public void naoDeveMudarDeEntregueParaAceito() {
         pedido.setPedidoEstado(PedidoEstadoEntregue.getInstance());
         assertFalse(pedido.aceitarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEntregueParaNegado(){
+    public void naoDeveMudarDeEntregueParaNegado() {
         pedido.setPedidoEstado(PedidoEstadoEntregue.getInstance());
         assertFalse(pedido.negarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEntregueParaPreparado(){
+    public void naoDeveMudarDeEntregueParaPreparado() {
         pedido.setPedidoEstado(PedidoEstadoEntregue.getInstance());
         assertFalse(pedido.prepararPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEntregueParaEnviado(){
+    public void naoDeveMudarDeEntregueParaEnviado() {
         pedido.setPedidoEstado(PedidoEstadoEntregue.getInstance());
         assertFalse(pedido.enviarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeEntregueParaCancelado(){
+    public void naoDeveMudarDeEntregueParaCancelado() {
         pedido.setPedidoEstado(PedidoEstadoEntregue.getInstance());
         assertFalse(pedido.cancelarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeCanceladoParaEfetuado(){
+    public void naoDeveMudarDeCanceladoParaEfetuado() {
         pedido.setPedidoEstado(PedidoEstadoCancelado.getInstance());
         assertFalse(pedido.efetuarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeCanceladoParaAceito(){
+    public void naoDeveMudarDeCanceladoParaAceito() {
         pedido.setPedidoEstado(PedidoEstadoCancelado.getInstance());
         assertFalse(pedido.aceitarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeCanceladoParaNegado(){
+    public void naoDeveMudarDeCanceladoParaNegado() {
         pedido.setPedidoEstado(PedidoEstadoCancelado.getInstance());
         assertFalse(pedido.negarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeCanceladoParaPreparado(){
+    public void naoDeveMudarDeCanceladoParaPreparado() {
         pedido.setPedidoEstado(PedidoEstadoCancelado.getInstance());
         assertFalse(pedido.prepararPedido());
     }
 
     @Test
-    public void naoDeveMudarDeCanceladoParaEnviado(){
+    public void naoDeveMudarDeCanceladoParaEnviado() {
         pedido.setPedidoEstado(PedidoEstadoCancelado.getInstance());
         assertFalse(pedido.enviarPedido());
     }
 
     @Test
-    public void naoDeveMudarDeCanceladoParaEntregue(){
+    public void naoDeveMudarDeCanceladoParaEntregue() {
         pedido.setPedidoEstado(PedidoEstadoCancelado.getInstance());
         assertFalse(pedido.entregarPedido());
     }
